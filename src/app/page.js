364a02a5c0a140ef0c0c7cd8f4c046da327a9534
Tóth Erik <totherikjanos@gmail.com>
2023-06-09 00:25:04 +0200

    Images loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import Hero from 'public/hero.png'
+//src="https://images.pexels.com/photos/15495789/pexels-photo-15495789/free-photo-of-felhok-auto-jarmu-multidezo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 export default function Home() {
   return (
     <div>
-      Home
+      <Image width={500} height={500} src={Hero} alt="" className={styles.img}/>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
-  return (
-    <div className={styles.container}>
-        <div>
-            2023 PracticePage. All rights reserved.
+    return (
+        <div className={styles.container}>
+            <div>
+                2023 PracticePage. All rights reserved.
+            </div>
+            <div className={styles.social}>
+                <Image src="/1.png" alt="PracticePage Facebook Account" width={15} height={15} className={styles.icon}/>
+                <Image src="/2.png" alt="PracticePage Instagram" width={15} height={15} className={styles.icon}/>
+                <Image src="/3.png" alt="PracticePage" width={15} height={15} className={styles.icon}/>
+                <Image src="/4.png" alt="PracticePage" width={15} height={15} className={styles.icon} />
+            </div>
         </div>
-        <div></div>
-    </div>
-  )
+    )
 }
 
 export default Footer
