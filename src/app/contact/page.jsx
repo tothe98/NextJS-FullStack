05756b577529bacb887e99bd9c15a6bec3c53ee4
Tóth Erik <totherikjanos@gmail.com>
2023-06-10 01:26:22 +0200

    Home, About, Contact pages styling

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <div className={styles.conatiner}>Contact</div>
+    <div className={styles.conatiner}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="contact" fill={true} className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder='name' className={styles.input} />
+          <input type="email" placeholder='email' className={styles.input} />
+          <textarea className={styles.textArea} placeholder='message' cols="30" rows="10"></textarea>
+          <Button url="#" text="Send" />
+        </form>
+
+      </div>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
+import Button from '@/components/Button/Button'
 //src="https://images.pexels.com/photos/15495789/pexels-photo-15495789/free-photo-of-felhok-auto-jarmu-multidezo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 export default function Home() {
   return (
-    <div>
-      <Image width={500} height={500} src={Hero} alt="" className={styles.img}/>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Welcome to my website!</h1>
+        <p className={styles.description}>This is a website made with Next.js and Tailwind CSS.</p>
+        <Button url="portfolio" text="Click me!"/>
+      </div>
+      <div className={styles.item}>
+        <Image width={500} height={500} src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
   )
 }
