c782cc497245dcd73ed6cba9a40e516d7078ce7b
Tóth Erik <totherikjanos@gmail.com>
2023-06-14 20:35:47 +0200

    Admin Page

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -16,5 +16,20 @@ export const GET = async (request, { params }) => {
 
         return new NextResponse("Database Error", { status: 500 });
     }
+}
 
+export const DELETE = async (request, { params }) => {
+
+    const { id } = params;
+
+    try {
+        await connect();
+
+        await Post.findByIdAndDelete(id);
+
+        return new NextResponse("Post has been deleted", { status: 200 });
+    } catch (error) {
+
+        return new NextResponse("Database Error", { status: 500 });
+    }
 }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,34 +4,15 @@ import styles from './page.module.css'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 const Dashboard = () => {
-  // const [data, setData] = useState([]);
-  // const [error, setError] = useState(false);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     setIsLoading(true);
-  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "no-store" });
-  //     if (!res.ok) {
-  //       setError(true);
-  //     }
-
-  //     const data = await res.json();
-
-  //     setData(data);
-  //     setIsLoading(false);
-  //   }
-  //   getData();
-  // }, [])
-
   const session = useSession();
 
   const router = useRouter();
 
-  //const fetcher = (...args) => fetch(...args).then(res => res.json())
-  //const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, mutate, error, isLoading } = useSWR(`/api/posts?username=${session?.data?.user.name}`, fetcher)
 
   if (session.status === "loading") {
     return (
@@ -43,9 +24,60 @@ const Dashboard = () => {
     router?.push('/dashboard/login');
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const description = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+    try {
+      await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, desc: description, img, content, username: session?.data?.user.name }),
+      })
+      mutate();
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      })
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   if (session.status === "authenticated") {
     return (
-      <div className={styles.container}>Dashboard</div>
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading ? "Loading..." : data?.map(post => {
+            return (
+              <div className={styles.post} key={post._id}>
+                <div className={styles.imgContainer}>
+                  <Image src={post.img} alt="" width={200} height={100} />
+                </div>
+                <h2 className={styles.postTitle}>{post.title}</h2>
+                <span className={styles.delete} onClick={() => handleDelete(post._id)}>X</span>
+              </div>
+            )
+          })}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Description" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea placeholder="Content" cols={30} rows={10} className={styles.textarea}></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
     )
   }
 }
