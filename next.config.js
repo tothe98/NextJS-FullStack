8fcfd7867b1910432f9f5848b947ca432340919f
Tóth Erik <totherikjanos@gmail.com>
2023-06-14 20:43:31 +0200

    Build Configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['images.pexels.com'],
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
